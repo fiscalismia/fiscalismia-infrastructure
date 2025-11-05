633ae278f66e9ef06a0c814a716ae76779fec4c8
hangrybear666 <hangrybear666+github@proton.me>
2025-11-05 10:38:27 +0100

    Cleans up naming schema of S3 access for App Lambda

--- terraform/aws/modules/application_lambda/app.lambda.iam.tf
+++ terraform/aws/modules/application_lambda/app.lambda.iam.tf
@@ -1,26 +1,26 @@
 # AWS IAM Role based policy for S3 access from lambda
-data "aws_iam_policy_document" "lambda_s3_iam_role_access" {
+data "aws_iam_policy_document" "lambda_s3_access" {
   statement {
-    sid       = "LambdaBucketReadAccess"
+    sid       = "BucketReadAccess"
     effect    = "Allow"
     actions   = ["s3:ListBucket"]
     resources = ["arn:aws:s3:::${var.s3_lambda_application_bucket}"]
   }
 
   statement {
-    sid       = "LambdaObjectReadWriteAccess"
+    sid       = "ObjectReadWriteAccess"
     effect    = "Allow"
     actions   = ["s3:GetObject", "s3:PutObject"]
     resources = ["arn:aws:s3:::${var.s3_lambda_application_bucket}/*"]
   }
 }
-resource "aws_iam_policy" "lambda_s3_iam_role_access" {
-  name        = "LambdaS3_IAM_Role_Access_${var.function_name}"
+resource "aws_iam_policy" "lambda_s3_access" {
+  name        = "S3_Access_${var.function_name}"
   description = "Allow Lambda functions to access S3"
-  policy      = data.aws_iam_policy_document.lambda_s3_iam_role_access.json
+  policy      = data.aws_iam_policy_document.lambda_s3_access.json
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_s3_policy_attachment" {
   role       = var.lambda_execution_role_name
-  policy_arn = aws_iam_policy.lambda_s3_iam_role_access.arn
+  policy_arn = aws_iam_policy.lambda_s3_access.arn
 }
