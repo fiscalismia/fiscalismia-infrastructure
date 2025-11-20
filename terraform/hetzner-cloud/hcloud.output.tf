6c912feb86149540bee2f81723bd73d7b4f8e8c7
hangrybear666 <hangrybear666+github@proton.me>
2025-11-20 19:31:23 +0100

    Fixes Hcloud to AWS Remote Terraform State

--- terraform/hetzner-cloud/hcloud.output.tf
+++ terraform/hetzner-cloud/hcloud.output.tf
@@ -16,3 +16,35 @@ output "server_ips_human_readable" {
     [""],
   ))
 }
+
+
+### output specifically for AWS REMOTE STATE to read from
+### marking as sensitive to hide from cli output since these are redundant
+output "fiscalismia_bastion_host_ipv4" {
+  value     = try(module.fiscalismia_bastion_host.server_ipv4_list[0], null)
+  sensitive = true
+}
+output "fiscalismia_loadbalancer_ipv4" {
+  value     = try(module.fiscalismia_loadbalancer.server_ipv4_list[0], null)
+  sensitive = true
+}
+output "fiscalismia_nat_gateway_ipv4" {
+  value     = try(module.fiscalismia_nat_gateway.server_ipv4_list[0], null)
+  sensitive = true
+}
+output "fiscalismia_demo_private_ipv4" {
+  value     = try(module.fiscalismia_demo.main_private_ipv4, null)
+  sensitive = true
+}
+output "fiscalismia_monitoring_private_ipv4" {
+  value     = try(module.fiscalismia_monitoring.main_private_ipv4, null)
+  sensitive = true
+}
+output "fiscalismia_frontend_private_ipv4" {
+  value     = try(module.fiscalismia_frontend.main_private_ipv4, null)
+  sensitive = true
+}
+output "fiscalismia_backend_private_ipv4" {
+  value     = try(module.fiscalismia_backend.main_private_ipv4, null)
+  sensitive = true
+}
