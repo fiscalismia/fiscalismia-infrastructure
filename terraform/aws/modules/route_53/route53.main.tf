43fbedbd2a35717ecad851a1cf5d1ff764bd876b
hangrybear666 <hangrybear666+github@proton.me>
2025-11-20 19:49:50 +0100

    Adds a full suite of route53 domain health checks

--- terraform/aws/modules/route_53/route53.main.tf
+++ terraform/aws/modules/route_53/route53.main.tf
@@ -81,24 +81,3 @@ resource "aws_route53_record" "type_A_monitoring_domain" {
   records = [var.loadbalancer_instance_ipv4]
   ttl     = 300
 }
-
-#        ___           ___          __        ___  __
-#  |__| |__   /\  |     |  |__|    /  ` |__| |__  /  ` |__/
-#  |  | |___ /~~\ |___  |  |  |    \__, |  | |___ \__, |  \
-resource "aws_route53_health_check" "root_domain_http_reachable" {
-  fqdn              = var.domain_name
-  port              = 80
-  type              = "HTTP"
-  resource_path     = "/"
-  failure_threshold = "5"
-  request_interval  = "30"
-}
-
-resource "aws_route53_health_check" "root_domain_https_reachable" {
-  fqdn              = var.domain_name
-  port              = 443
-  type              = "HTTPS"
-  resource_path     = "/"
-  failure_threshold = "5"
-  request_interval  = "30"
-}
