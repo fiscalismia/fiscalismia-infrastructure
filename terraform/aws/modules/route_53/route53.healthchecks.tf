36edba1754e338966607a0ff35bbc3e5cc0ecb1a
hangrybear666 <hangrybear666+github@proton.me>
2025-11-20 19:53:00 +0100

    Removes costly aws health checks as they go unused

--- terraform/aws/modules/route_53/route53.healthchecks.tf
+++ terraform/aws/modules/route_53/route53.healthchecks.tf
@@ -1,92 +1,95 @@
 #        ___           ___          __        ___  __
 #  |__| |__   /\  |     |  |__|    /  ` |__| |__  /  ` |__/
 #  |  | |___ /~~\ |___  |  |  |    \__, |  | |___ \__, |  \
-resource "aws_route53_health_check" "root_domain_http_reachable" {
-  fqdn              = var.domain_name
-  port              = 80
-  type              = "HTTP"
-  resource_path     = "/"
-  failure_threshold = "5"
-  request_interval  = "30"
-}
 
-resource "aws_route53_health_check" "root_domain_https_reachable" {
-  fqdn              = var.domain_name
-  port              = 443
-  type              = "HTTPS"
-  resource_path     = "/"
-  failure_threshold = "5"
-  request_interval  = "30"
-}
+# WARNING: EACH HEALTH CHECK COSTS ABOUT 0.50€ per month. And we currently do not use them
 
-resource "aws_route53_health_check" "demo_domain_http_reachable" {
-  fqdn              = "${var.demo_subdomain}.${var.domain_name}"
-  port              = 80
-  type              = "HTTP"
-  resource_path     = "/"
-  failure_threshold = "5"
-  request_interval  = "30"
-}
+# resource "aws_route53_health_check" "root_domain_http_reachable" {
+#   fqdn              = var.domain_name
+#   port              = 80
+#   type              = "HTTP"
+#   resource_path     = "/"
+#   failure_threshold = "5"
+#   request_interval  = "30"
+# }
 
-resource "aws_route53_health_check" "demo_domain_https_reachable" {
-  fqdn              = "${var.demo_subdomain}.${var.domain_name}"
-  port              = 443
-  type              = "HTTPS"
-  resource_path     = "/"
-  failure_threshold = "5"
-  request_interval  = "30"
-}
+# resource "aws_route53_health_check" "root_domain_https_reachable" {
+#   fqdn              = var.domain_name
+#   port              = 443
+#   type              = "HTTPS"
+#   resource_path     = "/"
+#   failure_threshold = "5"
+#   request_interval  = "30"
+# }
 
-resource "aws_route53_health_check" "demo_backend_domain_http_reachable" {
-  fqdn              = "${var.demo_backend_subdomains}.${var.domain_name}"
-  port              = 80
-  type              = "HTTP"
-  resource_path     = "/"
-  failure_threshold = "5"
-  request_interval  = "30"
-}
+# resource "aws_route53_health_check" "demo_domain_http_reachable" {
+#   fqdn              = "${var.demo_subdomain}.${var.domain_name}"
+#   port              = 80
+#   type              = "HTTP"
+#   resource_path     = "/"
+#   failure_threshold = "5"
+#   request_interval  = "30"
+# }
 
-resource "aws_route53_health_check" "demo_backend_domain_https_reachable" {
-  fqdn              = "${var.demo_backend_subdomains}.${var.domain_name}"
-  port              = 443
-  type              = "HTTPS"
-  resource_path     = "/"
-  failure_threshold = "5"
-  request_interval  = "30"
-}
+# resource "aws_route53_health_check" "demo_domain_https_reachable" {
+#   fqdn              = "${var.demo_subdomain}.${var.domain_name}"
+#   port              = 443
+#   type              = "HTTPS"
+#   resource_path     = "/"
+#   failure_threshold = "5"
+#   request_interval  = "30"
+# }
 
-resource "aws_route53_health_check" "backend_domain_http_reachable" {
-  fqdn              = "${var.backend_subdomain}.${var.domain_name}"
-  port              = 80
-  type              = "HTTP"
-  resource_path     = "/"
-  failure_threshold = "5"
-  request_interval  = "30"
-}
+# resource "aws_route53_health_check" "demo_backend_domain_http_reachable" {
+#   fqdn              = "${var.demo_backend_subdomains}.${var.domain_name}"
+#   port              = 80
+#   type              = "HTTP"
+#   resource_path     = "/"
+#   failure_threshold = "5"
+#   request_interval  = "30"
+# }
 
-resource "aws_route53_health_check" "backend_domain_https_reachable" {
-  fqdn              = "${var.backend_subdomain}.${var.domain_name}"
-  port              = 443
-  type              = "HTTPS"
-  resource_path     = "/"
-  failure_threshold = "5"
-  request_interval  = "30"
-}
+# resource "aws_route53_health_check" "demo_backend_domain_https_reachable" {
+#   fqdn              = "${var.demo_backend_subdomains}.${var.domain_name}"
+#   port              = 443
+#   type              = "HTTPS"
+#   resource_path     = "/"
+#   failure_threshold = "5"
+#   request_interval  = "30"
+# }
 
-resource "aws_route53_health_check" "monitoring_domain_http_reachable" {
-  fqdn              = "${var.monitoring_subdomain}.${var.domain_name}"
-  port              = 80
-  type              = "HTTP"
-  resource_path     = "/"
-  failure_threshold = "5"
-  request_interval  = "30"
-}
+# resource "aws_route53_health_check" "backend_domain_http_reachable" {
+#   fqdn              = "${var.backend_subdomain}.${var.domain_name}"
+#   port              = 80
+#   type              = "HTTP"
+#   resource_path     = "/"
+#   failure_threshold = "5"
+#   request_interval  = "30"
+# }
 
-resource "aws_route53_health_check" "monitoring_domain_https_reachable" {
-  fqdn              = "${var.monitoring_subdomain}.${var.domain_name}"
-  port              = 443
-  type              = "HTTPS"
-  resource_path     = "/"
-  failure_threshold = "5"
-  request_interval  = "30"
-}
+# resource "aws_route53_health_check" "backend_domain_https_reachable" {
+#   fqdn              = "${var.backend_subdomain}.${var.domain_name}"
+#   port              = 443
+#   type              = "HTTPS"
+#   resource_path     = "/"
+#   failure_threshold = "5"
+#   request_interval  = "30"
+# }
+
+# resource "aws_route53_health_check" "monitoring_domain_http_reachable" {
+#   fqdn              = "${var.monitoring_subdomain}.${var.domain_name}"
+#   port              = 80
+#   type              = "HTTP"
+#   resource_path     = "/"
+#   failure_threshold = "5"
+#   request_interval  = "30"
+# }
+
+# resource "aws_route53_health_check" "monitoring_domain_https_reachable" {
+#   fqdn              = "${var.monitoring_subdomain}.${var.domain_name}"
+#   port              = 443
+#   type              = "HTTPS"
+#   resource_path     = "/"
+#   failure_threshold = "5"
+#   request_interval  = "30"
+# }
