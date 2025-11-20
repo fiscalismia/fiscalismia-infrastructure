f3226429e6d51681e8f7200ac36f93787c802b88
hangrybear666 <hangrybear666+github@proton.me>
2025-11-20 19:26:11 +0100

    Fixes malformed output in hcloud

--- terraform/hetzner-cloud/hcloud.output.tf
+++ terraform/hetzner-cloud/hcloud.output.tf
@@ -3,14 +3,16 @@ output "server_ips_human_readable" {
 
   value = join("\n", concat(
     [""],
-    ["############ PUBLIC INFRASTRUCTURE #########"],
+    ["############ PUBLIC INFRASTRUCTURE ############"],
     try(module.fiscalismia_bastion_host.server_ipv4_ascii_list, []),
     try(module.fiscalismia_loadbalancer.server_ipv4_ascii_list, []),
     try(module.fiscalismia_nat_gateway.server_ipv4_ascii_list, []),
-    ["############ PRIVATE INFRASTRUCTURE ########"],
-    format(" > %-30s %s", "Fiscalismia-Demo:", module.fiscalismia_demo.main_private_ipv4),
-    format(" > %-30s %s", "Fiscalismia-Monitoring:", module.fiscalismia_monitoring.main_private_ipv4),
-    format(" > %-30s %s", "Fiscalismia-Frontend:", module.fiscalismia_frontend.main_private_ipv4),
-    format(" > %-30s %s", "Fiscalismia-Backend:", module.fiscalismia_backend.main_private_ipv4),
+    [""],
+    ["############ PRIVATE INFRASTRUCTURE ###########"],
+    [format(" > %-30s %s", "Fiscalismia-Demo:", module.fiscalismia_demo.main_private_ipv4)],
+    [format(" > %-30s %s", "Fiscalismia-Monitoring:", module.fiscalismia_monitoring.main_private_ipv4)],
+    [format(" > %-30s %s", "Fiscalismia-Frontend:", module.fiscalismia_frontend.main_private_ipv4)],
+    [format(" > %-30s %s", "Fiscalismia-Backend:", module.fiscalismia_backend.main_private_ipv4)],
+    [""],
   ))
 }
