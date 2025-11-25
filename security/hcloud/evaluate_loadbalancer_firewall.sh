af4d6ed19622502566d3c34428fc26e73609f1ad
hangrybear666 <hangrybear666+github@proton.me>
2025-11-25 23:58:26 +0100

    asf

--- security/hcloud/evaluate_loadbalancer_firewall.sh
+++ security/hcloud/evaluate_loadbalancer_firewall.sh
@@ -3,7 +3,6 @@
 # example list extracted from terraform/hetzner-cloud/config/network.private.ips.yml with yq
 # virtual_network_gateway_demo_net:172.20.0.1 virtual_network_gateway_production_net:172.24.0.1 fiscalismia_demo_private_ipv4:172.20.0.2 fiscalismia_monitoring_private_ipv4:172.24.0.2 fiscalismia_frontend_private_ipv4:172.24.0.3 fiscalismia_backend_private_ipv4:172.24.0.4 fiscalismia_bastion_host_private_ipv4_demo_net:172.20.1.2 fiscalismia_bastion_host_private_ipv4_production_net:172.24.1.2 fiscalismia_loadbalancer_private_ipv4_demo_net:172.20.1.3 fiscalismia_loadbalancer_private_ipv4_production_net:172.24.1.3 fiscalismia_nat_gateway_private_ipv4_demo_net:172.20.1.4 fiscalismia_nat_gateway_private_ipv4_production_net:172.24.1.4
 
-
 if [ -z "$1" ]; then
   echo "ERROR: Instance list parameter not provided." >&2
   exit 1
