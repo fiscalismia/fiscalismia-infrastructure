774978f3ec5bf8bbef1f37c55e2addb5bcceb558
hangrybear666 <hangrybear666+github@proton.me>
2025-12-05 21:29:09 +0100

    Successful DNS route 53 validation supposedly

--- terraform/hetzner-cloud/modules/hcloud_server/user_data/tools/fetch-and-validate-tls-certificates.sh
+++ terraform/hetzner-cloud/modules/hcloud_server/user_data/tools/fetch-and-validate-tls-certificates.sh
@@ -2,6 +2,7 @@
 ##################################################################################################################
 # We use certbot with the route53 plugin with scoped down AWS credentials to validate our TLS certificates
 # Certbot queries letsencrypt for a certificate and then adds a TXT Record to the AWS hosted zone's for validation
+# certbot sets up automatic renewal and by default certificates are valid for only 90 days.
 # PARAM $1 is the domain or subdomnain for tls certificate
 # PARAM $2 optional second domain to fetch and validate. e.g. the demo instance uses two separate certs.
 ##################################################################################################################
@@ -20,14 +21,25 @@ if [[ "$2" ]] then
     echo $1 && echo $2
     sudo certbot certonly \
     --dns-route53 \
-    --dns-route53-credentials /root/.aws/credentials \
+    --non-interactive \
+    --agree-tos \
+    --key-type ecdsa \
     -d $1 \
     -d $2
 else
     echo "Requesting single tls certificate for $1"
     sudo certbot certonly \
     --dns-route53 \
-    --dns-route53-credentials /root/.aws/credentials \
+    --non-interactive \
+    --agree-tos \
+    --key-type ecdsa \
     -d $1
 fi
 
+# list generated certificates
+echo "Listing created certificates"
+sudo certbot certificates
+
+# simulate certificate renewal
+echo "Simulating certificate renewal"
+sudo certbot renew --dry-run
