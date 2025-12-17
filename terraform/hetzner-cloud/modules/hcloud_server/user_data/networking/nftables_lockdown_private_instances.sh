c0c8ef21f983cf614ec3e75228a33af6b3f5fd1f
hangrybear666 <hangrybear666+github@proton.me>
2025-12-17 21:06:24 +0100

    Adds nftables podman networking to oci container orchestration

--- terraform/hetzner-cloud/modules/hcloud_server/user_data/networking/nftables_lockdown_private_instances.sh
+++ terraform/hetzner-cloud/modules/hcloud_server/user_data/networking/nftables_lockdown_private_instances.sh
@@ -9,8 +9,8 @@
 # PARAM $4 is the virtual network gateway used as the next-hop target for private egress to NAT-Gateway
 # PARAM $5 Comma-separated list of podman ports for local NAT port forwarding for ingress & egress, e.g. '443,8443,3002,5432'
 # PARAM $6 OPTIONAL Parameter to allow an additional port for ingress on the demo instance running frontend + backend
-When a new network is created with a podman network create command, and no subnet is given with the --subnet option, Podman starts picking a free subnet from 10.89.0.0/24 to 10.255.255.0/24
-# e.g. ./scripts/nftables_lockdown_private_instances.sh 172.20.1.3 172.20.1.2 172.20.1.4 172.20.0.1
+# When a new network is created with a podman network create command, and no subnet is given with the --subnet option, Podman starts picking a free subnet from 10.89.0.0/24 to 10.255.255.0/24
+# e.g. ./scripts/nftables_lockdown_private_instances.sh 172.20.1.3 172.20.1.2 172.20.1.4 172.20.0.1 "443,8443,5432" 8443
 ##############################################################################################################################
 
 ### PODMAN NETWORKING INFO ###
@@ -21,6 +21,7 @@ When a new network is created with a podman network create command, and no subne
 export PODMAN_NETWORK_CIDR="10.89.0.0/22"
 
 # wait for the private network interface to initialize.
+echo "Locking down private instance via minimal required nftables firewall in 60 seconds..."
 sleep 60
 
 export LOADBALANCER_PRIVATE_IP="$1"
@@ -32,16 +33,16 @@ export PODMAN_LISTENER_PORT_LIST="{$PODMAN_LISTENER_PORT_LIST_RAW}"
 export TABLE_NAME='lockdown_private_instances'
 export CONFIG_PATH='/etc/sysconfig/nftables.conf'
 
-if [[ -z "$1" ]] || [[ -z "$2" ]] || [[ -z "$3" ]] || [[ -z "$4" ]] || [[ -z "$5" ]] || [[ -z "$6" ]]; then
+if [[ -z "$1" ]] || [[ -z "$2" ]] || [[ -z "$3" ]] || [[ -z "$4" ]] || [[ -z "$5" ]]; then
     echo "Error: Missing required parameters."
-    echo "Usage: $0 <LOADBALANCER_PRIVATE_IP> <BASTION_HOST_PRIVATE_IP> <NAT_GATEWAY_PRIVATE_IP> <VIRTUAL_NETWORK_GATEWAY>"
+    echo "Usage: $0 <LOADBALANCER_PRIVATE_IP> <BASTION_HOST_PRIVATE_IP> <NAT_GATEWAY_PRIVATE_IP> <VIRTUAL_NETWORK_GATEWAY> <PODMAN_LISTENER_PORT_LIST_RAW> <OPTIONAL_BACKEND_DEMO_INGRESS_PORT>"
     exit 1
 fi
 
 if [[ "$6" ]]; then
-    echo "Allowing additional port for demo instance: $5"
+    echo "Allowing additional port for demo instance: $6"
     # TODO remove port 80 in production
-    export LB_INGRESS_PORTS="{80,443,$5}"
+    export LB_INGRESS_PORTS="{80,443,$6}"
 else
     # TODO remove port 80 in production
     export LB_INGRESS_PORTS="{80,443}"
@@ -115,7 +116,7 @@ table ip $TABLE_NAME {
         ct state established,related accept
 
         # Allow podman interinter-container communication within the entire network subnet CIDR
-        ip saddr $PODMAN_NETWORK_CIDR ip daddr $PODMAN_NETWORK_CIDR tcp accept
+        ip saddr $PODMAN_NETWORK_CIDR ip daddr $PODMAN_NETWORK_CIDR ip protocol tcp accept
 
         # Allow ingress from loadbalancer to podman network subnet CIDR on specified ports
         ip daddr $PODMAN_NETWORK_CIDR tcp dport $PODMAN_LISTENER_PORT_LIST ct state new accept
