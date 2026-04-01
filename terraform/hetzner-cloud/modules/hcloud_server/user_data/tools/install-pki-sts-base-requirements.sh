07d1b07c76172c779d320a5dcce078e18c545d2e
hangrybear666 <hangrybear666+github@proton.me>
2026-04-01 20:26:10 +0200

    Pins aws signing helper version

--- terraform/hetzner-cloud/modules/hcloud_server/user_data/tools/install-pki-sts-base-requirements.sh
+++ terraform/hetzner-cloud/modules/hcloud_server/user_data/tools/install-pki-sts-base-requirements.sh
@@ -2,8 +2,17 @@
 
 ################################ INFO ###############################################################################################
 # This file installs required tools for generating the pki infrastructure and certificates during webservice deployment pipeline runs
-# e.g. ./scripts/install-pki-sts-base-requirements.sh
+# PARAM $1 is the release version of the X86_64/Linux/Amzn2023/aws_signing_helper
+# e.g. ./scripts/install-pki-sts-base-requirements.sh 1.8.0
 #####################################################################################################################################
+
+AWS_SIGNING_HELPER_VERSION="$1"
+
+if [[ -z "$1" ]]; then
+    echo "Error: Missing required parameters."
+    echo "Usage: $0 <AWS_SIGNING_HELPER_VERSION>"
+    exit 1
+fi
 
 # Install step-ca cli for setting up pki infrastructure
 cat <<EOT | sudo tee /etc/yum.repos.d/smallstep.repo > /dev/null
@@ -20,7 +29,7 @@ sudo dnf install -y --quiet step-cli
 
 # Download and copy AWS signing helper binary for renewing short-lived STS credentials via credential_process by authenticating with X.509 PKI Certs
 cd /tmp
-curl -sLO https://rolesanywhere.amazonaws.com/releases/1.8.0/X86_64/Linux/Amzn2023/aws_signing_helper
+curl -sLO https://rolesanywhere.amazonaws.com/releases/${AWS_SIGNING_HELPER_VERSION}/X86_64/Linux/Amzn2023/aws_signing_helper
 mv /tmp/aws_signing_helper /usr/local/bin/aws_signing_helper
 sudo chmod 744 /usr/local/bin/aws_signing_helper
 
