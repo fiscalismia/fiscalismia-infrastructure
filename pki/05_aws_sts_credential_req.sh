b4bc4f95cae38932db0071570300e622cb2bcb9f
hangrybear666 <hangrybear666+github@proton.me>
2026-04-01 21:41:29 +0200

    Fixes ssh command syntax

--- pki/05_aws_sts_credential_req.sh
+++ pki/05_aws_sts_credential_req.sh
@@ -8,7 +8,7 @@
 # PARAM $2 the arn:aws:rolesanywhere:region-account-id:profile/id
 # PARAM $3 the arn:aws:iam::account-id:role/rolename
 # PARAM $4 is the id of the secret containing the .env file in eu-central-1 region
-# e.g. ./scripts/05_aws_sts_credential_req.sh arn:aws:rolesanywhere:eu-central-1:010928217051:trust-anchor/c3c0f28b-7779-4acb-b698-87e72a46b9db arn:aws:rolesanywhere:eu-central-1:010928217051:profile/f0e050f8-2e78-411d-8c39-a2938475fbf5 arn:aws:iam::010928217051:role/HetznerPKI-Secret-Retrieval-Role
+# e.g. ./scripts/05_aws_sts_credential_req.sh arn:aws:rolesanywhere:eu-central-1:010928217051:trust-anchor/c3c0f28b-7779-4acb-b698-87e72a46b9db arn:aws:rolesanywhere:eu-central-1:010928217051:profile/f0e050f8-2e78-411d-8c39-a2938475fbf5 arn:aws:iam::010928217051:role/HetznerPKI-Secret-Retrieval-Role fiscalismia-backend/.env
 ##############################################################################################################################
 
 export LEAF_CERT="/etc/pki/iam-anywhere/end-entity-cert.pem"
@@ -44,7 +44,7 @@ CONFIG
 
 export AWS_PROFILE="hetzner-pki"
 aws secretsmanager get-secret-value \
-  --profile hetzner-pki \
+  --profile $AWS_PROFILE \
   --secret-id $SECRET_ID \
   --region eu-central-1 \
   --output text \
