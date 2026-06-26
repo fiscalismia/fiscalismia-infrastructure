36326717ca989d2d5d00fc48c4a6ad9bf63df525
hangrybear666 <hangrybear666+github@proton.me>
2026-06-26 20:28:53 +0200

    Merges forecast budget notifications

--- terraform/aws/modules/cost_budget/cost.budget.alarm.tf
+++ terraform/aws/modules/cost_budget/cost.budget.alarm.tf
@@ -4,7 +4,7 @@ locals {
 }
 
 # Invokes destruction lambda, that actually destroys resources
-resource "aws_budgets_budget" "total_actual" {
+resource "aws_budgets_budget" "total_actual_destruction" {
   name              = var.cost_budget_alarm_total_actual_name
   budget_type       = "COST"
   limit_amount      = local.ACTUAL_VALUE
@@ -23,8 +23,8 @@ resource "aws_budgets_budget" "total_actual" {
 }
 
 # Invokes email sending that only notifies the user via mail
-resource "aws_budgets_budget" "total_forecasted_mail" {
-  name              = var.cost_budget_alarm_total_forecast_mail_name
+resource "aws_budgets_budget" "total_forecasted_notifaction" {
+  name              = var.cost_budget_alarm_total_forecast_name
   budget_type       = "COST"
   limit_amount      = local.FORECASTED_VALUE
   limit_unit        = "USD"
@@ -32,25 +32,16 @@ resource "aws_budgets_budget" "total_forecasted_mail" {
   time_period_end   = "2050-01-31_00:00"
   time_unit         = "MONTHLY"
 
+  # Email notification at 80% threshold
   notification {
     comparison_operator        = "GREATER_THAN"
-    threshold                  = 100
+    threshold                  = 80
     threshold_type             = "PERCENTAGE"
     notification_type          = "FORECASTED"
     subscriber_email_addresses = [var.forecasted_budget_notification_email]
   }
-}
-
-# Invokes destruction lambda, that only notifies the user via SNS
-resource "aws_budgets_budget" "total_forecasted_telegram_notification" {
-  name              = var.cost_budget_alarm_total_forecast_telegram_name
-  budget_type       = "COST"
-  limit_amount      = local.FORECASTED_VALUE
-  limit_unit        = "USD"
-  time_period_start = "2025-11-01_00:00"
-  time_period_end   = "2050-01-31_00:00"
-  time_unit         = "MONTHLY"
-
+  
+  # SNS notification (for Lambda/Telegram) at 100% threshold
   notification {
     comparison_operator        = "GREATER_THAN"
     threshold                  = 100
--- terraform/aws/modules/cost_budget/cost.budget.variables.tf
+++ terraform/aws/modules/cost_budget/cost.budget.variables.tf
@@ -1,5 +1,4 @@
 variable "sns_topic_arn_budget_limit_exceeded" {}
 variable "forecasted_budget_notification_email" {}
 variable "cost_budget_alarm_total_actual_name" {}
-variable "cost_budget_alarm_total_forecast_mail_name" {}
-variable "cost_budget_alarm_total_forecast_telegram_name" {}
+variable "cost_budget_alarm_total_forecast_name" {}
